0b8853bdc3221f264005826a069554ad2498d46a
Eder2016Eucatur <antonioeder007@gmail.com>
2018-10-08 17:42:55 -0400

    Corrigindo o projeto

---
+++ Vue/vuex-lista-de-tarefas/app_tarefas/src/loja/estado.js
@@ -0,0 +1,7 @@
+export default{
+  tarefas:[],
+  pessoa:{
+    nome:'João bobo',
+    cpf:"001.511.902-11"
+  }
+}
---
+++ Vue/vuex-lista-de-tarefas/app_tarefas/src/store.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import estado  from './loja/estado'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    estado
+  },
+  mutations: {
+
+  },
+  actions: {
+
+  }
+})
